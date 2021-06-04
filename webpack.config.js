caf6ed7dda7f8515db972230d77c7a1d2216dabc
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-06-04 08:13:32 +0530

    config : updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
-module.exports = {
+const clientConfig = {
+    target: "web",
     mode: "production",
     entry: "./src/index.js",
     output: {
@@ -45,3 +46,53 @@ module.exports = {
         },
     },
 };
+
+
+const serverConfig = {
+    target: "node",
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.node.js",
+        path: path.resolve(__dirname, "dist"),
+        library: {
+            name: "zeal-ui",
+            type: "umd",
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/react"],
+                    plugins: [
+                        "transform-class-properties",
+                        "transform-object-rest-spread",
+                        "babel-plugin-styled-components",
+                    ],
+                },
+            },
+            {
+                test: /\.css$/i,
+                loader: "css-loader",
+                options: {
+                    modules: true,
+                },
+            },
+        ],
+    },
+    externals: {
+        react: "react",
+        "react-dom": "react-dom",
+        "styled-components": {
+            commonjs: "styled-components",
+            commonjs2: "styled-components",
+            amd: "styled-components",
+        },
+    },
+};
+
+module.exports = [clientConfig, serverConfig];
