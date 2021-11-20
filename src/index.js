ea0e899b05e59bfc906ac5442b0f5229e2b554e3
M V Ganesh Kumar <mvganeshkumar06@outlook.com>
2021-11-20 21:18:13 +0530

    refactor : updated exports

--- src/index.js
+++ src/index.js
@@ -1,73 +1,67 @@
 /* Provider */
 
-export { default as ZealProvider } from "./context/ZealProvider";
+export { default as ZealProvider } from './context/ZealProvider';
 
 /* Hooks */
 
-export { default as useMediaQuery } from "./hooks/useMediaQuery";
+export { default as useClickAway } from './hooks/use-click-away';
 
-export { default as useNotify } from "./hooks/useNotify";
+export { default as useColorMode } from './hooks/use-color-mode';
 
-export { default as useStyles } from "./hooks/useStyles";
+export { default as useMediaQuery } from './hooks/use-media-query';
 
-export { default as useStyleContext } from "./hooks/useStyleContext";
+export { default as useNotify } from './hooks/use-notify';
 
-export { default as useThemeContext } from "./hooks/useThemeContext";
+/* Utility */
+
+export { default as createCustomTheme } from './theme/create-custom-theme';
 
 /* Components */
 
-export { default as Alert } from "./components/alert/Alert";
+export { default as Alert } from './components/alert/Alert';
 
-export { default as Avatar } from "./components/avatar/Avatar";
+export { default as Avatar } from './components/avatar/Avatar';
 
-export { default as AvatarBadge } from "./components/avatar/AvatarBadge";
+export { default as AvatarBadge } from './components/avatar/AvatarBadge';
 
-export { default as Badge } from "./components/badge/Badge";
+export { default as Badge } from './components/badge/Badge';
 
-export { default as Button } from "./components/button/Button";
+export { default as Button } from './components/button/Button';
 
-export { default as Checkbox } from "./components/checkbox/Checkbox";
+export { default as ColorModeSwitch } from './components/color-mode-switch/ColorModeSwitch';
 
-export { default as Container } from "./components/container/Container";
+export { default as Container } from './components/container/Container';
 
-export { default as Divider } from "./components/divider/Divider";
+export { default as Divider } from './components/divider/Divider';
 
-export { default as Footer } from "./components/footer/Footer";
+export { default as Drawer } from './components/drawer/Drawer';
 
-export { default as Grid } from "./components/grid/Grid";
+export { default as Grid } from './components/grid/Grid';
 
-export { default as Header } from "./components/header/Header";
+export { default as Image } from './components/image/Image';
 
-export { default as Image } from "./components/image/Image";
+export { default as InlineCode } from './components/inline-code/InlineCode';
 
-export { default as InlineCode } from "./components/inline-code/InlineCode";
+export { default as Input } from './components/input/Input';
 
-export { default as Input } from "./components/input/Input";
+export { default as Layout } from './components/layout/Layout';
 
-export { default as Layout } from "./components/layout/Layout";
+export { default as List } from './components/list/List';
 
-export { default as List } from "./components/list/List";
+export { default as ListItem } from './components/list/ListItem';
 
-export { default as ListItem } from "./components/list/ListItem";
+export { default as Menu } from './components/menu/Menu';
 
-export { default as Menu } from "./components/menu/Menu";
+export { default as Modal } from './components/modal/Modal';
 
-export { default as Modal } from "./components/modal/Modal";
+export { default as Skeleton } from './components/skeleton/Skeleton';
 
-export { default as Radio } from "./components/radio/Radio";
+export { default as SlideShow } from './components/slideshow/Slideshow';
 
-export { default as Sidebar } from "./components/sidebar/Sidebar";
+export { default as Spinner } from './components/spinner/Spinner';
 
-export { default as SlideShow } from "./components/slideshow/Slideshow";
+export { default as Switch } from './components/switch/Switch';
 
-export { default as Span } from "./components/span/Span";
+export { default as Text } from './components/text/Text';
 
-export { default as Spinner } from "./components/spinner/Spinner";
-
-export { default as Switch } from "./components/switch/Switch";
-
-export { default as Text } from "./components/text/Text";
-
-export { default as ThemeSwitch } from "./components/theme-switch/ThemeSwitch";
-
-export { default as Toast } from "./components/toast/Toast";
+export { default as Toast } from './components/toast/Toast';
